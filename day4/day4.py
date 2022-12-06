bbc73fa288de12565fb2f98e25262de3ed895687
Hampton Walker <hamptonwwalker@gmail.com>
2022-12-05 21:18:45 -0700

    added comments to day4 solution

--- day4/day4.py
+++ day4/day4.py
@@ -7,12 +7,12 @@ for line in l:
   x1, x2 = p1.split("-")
   y1, y2 = p2.split("-")
 
-  # if int(x1, base=0) <= int(y1, base=0) and int(x2, base=0) >= int(y2, base=0):
+  # if int(x1, base=0) <= int(y1, base=0) and int(x2, base=0) >= int(y2, base=0): # If x range encapsulates y range
   #   count += 1
-  # elif int(y1, base=0) <= int(x1, base=0) and int(y2, base=0) >= int(x2, base=0):
+  # elif int(y1, base=0) <= int(x1, base=0) and int(y2, base=0) >= int(x2, base=0): # if y range encapsul;ates x range
   #   count += 1
-  if (int(x2, base=0) >= int(y1, base=0)) and (int(x2, base=0) <= int(y2, base=0)):
+  if (int(x2, base=0) >= int(y1, base=0)) and (int(x2, base=0) <= int(y2, base=0)): # endpoint of x is in range of y
     count += 1
-  elif (int(y2, base=0) >= int(x1, base=0)) and (int(y2, base=0) <= int(x2, base=0)):
+  elif (int(y2, base=0) >= int(x1, base=0)) and (int(y2, base=0) <= int(x2, base=0)): # endpoint of y is in range of x
     count += 1
 print(count)
